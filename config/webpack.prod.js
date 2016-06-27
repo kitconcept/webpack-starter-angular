8c7dbda2960c575efdd7b8dbf50e4a6fb346d362
Timo Stollenwerk <tisto@plone.org>
2016-06-27 19:12:19 +0200

    Add UglifyJs plugin to prod env.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,6 +5,7 @@ const commonConfig = require('./webpack.common.js');
 // webpack plugins
 const WebpackMd5Hash = require('webpack-md5-hash');
 const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 
 
 module.exports = webpackMerge(commonConfig, {
@@ -91,7 +92,37 @@ module.exports = webpackMerge(commonConfig, {
      * See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
      * See: https://github.com/webpack/docs/wiki/optimization#deduplication
      */
-    new DedupePlugin()
+    new DedupePlugin(),
+
+    /**
+     * Plugin: UglifyJsPlugin
+     * Description: Minimize all JavaScript output of chunks.
+     * Loaders are switched into minimizing mode.
+     *
+     * See: https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+     */
+    // NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
+    new UglifyJsPlugin({
+      // beautify: true, //debug
+      // mangle: false, //debug
+      // dead_code: false, //debug
+      // unused: false, //debug
+      // deadCode: false, //debug
+      // compress: {
+      //   screw_ie8: true,
+      //   keep_fnames: true,
+      //   drop_debugger: false,
+      //   dead_code: false,
+      //   unused: false
+      // }, // debug
+      // comments: true, //debug
+
+
+      beautify: false, //prod
+      mangle: { screw_ie8 : true }, //prod
+      compress: { screw_ie8: true }, //prod
+      comments: false //prod
+    }),
 
   ]
 
