58da398ea9121f7dd2a8f2b103f1705bd23a489f
Timo Stollenwerk <tisto@plone.org>
2016-06-27 11:49:26 +0200

    Add some notes for source-map setting.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,5 +2,16 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
+
+  /**
+   * Developer tool to enhance debugging.
+   *
+   * The 'source-map' settings is meant to be used in production only. It
+   * splits the source map in a separate file and it is slow to compute.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#devtool
+   * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+   */
   devtool: 'source-map',
+
 });
